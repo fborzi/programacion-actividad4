891198aa64dd1e98e820efcc0ace7d07a353c48e
quevedocandela05@gmail.com <quevedocandela05@gmail.com>
2025-09-19 21:27:51 -0300

    [SCRUM-12] feat : resolvi el primer ejerciico que pide indexar una lista, lo resolvi mediante un for i in b

---
+++ pareja12/ejercicio1.py
@@ -0,0 +1,12 @@
+''' EJERCI8CIO 1 '''
+
+b= [4 , 'palabra' , [0 , 1] , 9.6 , False]
+
+verificar= [9.6 , 0 , False , [0,1] , 4.0 , 'p']
+
+for i in b:
+    if i in verificar:
+        print(i, 'pertenece a su lista')
+    else:
+        print(i, 'no pertenece a su lista')
+
