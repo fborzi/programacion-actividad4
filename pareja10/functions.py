ebe4375588406ef9e6f21f489255559760efd3aa
yemesesteban@gmail.com <yemesesteban@gmail.com>
2025-09-19 21:19:44 -0300

    [SCRUM-19]feat: Added functions for exercise 15
    In this implementation the methods getCity(), getState() and getPeopleInStateAmmount are for the exercise 15

---
+++ pareja10/functions.py
@@ -0,0 +1,47 @@
+def getCity(people, dni):
+    """
+    It´s a function that returns a person´s city name if dni matches
+    with the person´s dni. If not, it returns "City not found" prhase.
+    Eparams - people: list - people list, dni: int - person´s dni
+    @return - str
+    """
+    for person in people:
+        if dni in person:
+            return person[2]
+        
+    return "City not found."
+
+def getState(people, cities, dni):
+    """
+    It´s a function that returns a person´s state name if dni matches
+    with the person´s dni. If not, it returns "State not found" prhase.
+    Eparams - people: list - people list, cities: list - cities list, dni: int - person´s dni
+    @return - str
+    """
+    city = getCity(people, dni)
+
+    for personState in cities:
+        if city in personState:
+            return personState[1]
+        
+    return "State not found."
+
+def getPeopleInStateAmmount(people, cities, state):
+    """
+    It´s a function that returns the people ammount that live in a State.   
+    Eparams - people: list - people list, cities: list - cities list, state: str - state to check
+    @return - int
+    """
+
+    peopleAmmount = 0
+    personState = ""
+    personDni = 0
+
+    for person in people:
+        personDni = person[1]
+        personState = getState(people, cities, personDni)
+
+        if personState == state:
+            peopleAmmount += 1
+
+    return peopleAmmount
