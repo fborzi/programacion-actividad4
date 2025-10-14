bff6f0eef57f202d65b5583caa9f966260fc94dc
Federico Della Savia <federicodellasavia@gmail.com>
2025-10-13 21:28:51 -0300

    SCRUM-69: ejercicio1

---
+++ pareja08/ejercicio1.py
@@ -0,0 +1,28 @@
+#Dada la lista b = [ 4, "palabra", [0, 1], 9.6, False ]:
+#A) Utilizar una instrucción para determinar si los siguientes elementos pertenecen a la lista:
+#9.6 0 False [0, 1] 4.0 "p"
+#B) Utilizar una instrucción para determinar en qué posición (índice) se encuentra el elemento [0, 1].
+b=[4, "palabra", [0,1], 9.6, False]
+verificar=[9.6, 0, [0,1], 4.0, False, "p"]
+encontrados=[]
+noPertenecen=[]
+def pertenecer(elemento,lista):
+    for item in lista:
+        if item==elemento and type(item)==type(elemento):
+            return True;
+        
+    return False;
+for elemento in verificar:
+    if pertenecer(elemento,b):
+        encontrados.append(elemento)
+    else:
+        noPertenecen.append(elemento)
+
+if len(encontrados)==len(b):
+    print("Se encontraron todos los elementos ")
+    print(encontrados)
+else:
+    print("No se encontraron todos los elementos")
+    print("Solo estos:", encontrados)
+    print("Los que no se encontraron fueron: ", noPertenecen)
+            
