ab1ab60e9c4f0c6cb480c7ea7abb98ccfd454f47
quevedocandela <quevedocandela05@gmail.com>
2025-10-31 21:41:39 -0300

    En el punto A se verifico la existencia de elementos de una lista y en el punto B se busco el indice de un elemento

--- pareja12/ejercicio1.py
+++ pareja12/ejercicio1.py
@@ -1,5 +1,5 @@
 ''' EJERCI8CIO 1 '''
-
+'''A'''
 b= [4 , 'palabra' , [0 , 1] , 9.6 , False]
 
 verificar= [9.6 , 0 , False , [0,1] , 4.0 , 'p']
@@ -10,3 +10,7 @@ for i in b:
     else:
         print(i, 'no pertenece a su lista')
 
+'''B'''
+for i in b: 
+    if i == [0,1]:
+        print(b.index(i))
