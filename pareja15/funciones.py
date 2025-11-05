d5d1a2cad5adf57322e4b33882ea89f34695b754
Lourdes999 <lourdesmelo999@gmail.com>
2025-11-05 20:32:09 -0300

    Ejercicio 2. Instrucción para contar cuantas veces aparece un número

---
+++ pareja15/funciones.py
@@ -0,0 +1,5 @@
+def listaL(nombre : str) -> list:
+    """Función que recibe un nombre y devuelve una lista con cada letra del nombre como un elemento de la lista.
+     Args:
+    nombre (str): Nombre a convertir en lista de letras."""
+    return [letra for letra in nombre]     
