ffb8042a22d607560f7b27360e9e02d80a82c73d
Lourdes999 <lourdesmelo999@gmail.com>
2025-11-06 00:08:24 -0300

    Ejercicio 8. Simulador del juego piedra, papel o tijera

--- pareja15/funciones.py
+++ pareja15/funciones.py
@@ -5,9 +5,29 @@
 """
 def minimo_elemento(lista):
     if not lista:
-        return None  # Manejar lista vacía
+        return None   
     minimo = lista[0]
     for elemento in lista:
         if elemento < minimo:
             minimo = elemento
     return minimo
+
+#Ejercicio8
+"""Función que simula el juego de piedra, papel o tijera.
+ @param uno: Elección del primer jugador ('piedra', 'papel' o 'tijera').
+ @param dos: Elección del segundo jugador ('piedra', 'papel' o 'tijera').
+ return: 1 si gana el primer jugador, 2 si gana el segundo jugador, 0 en caso de empate.
+"""
+def piedra_papel_tijera(uno, dos):
+    uno = uno.lower()
+    dos = dos.lower()
+    
+    if uno == dos:
+        return 0
+    
+    if (uno == 'piedra' and dos == 'tijera') or \
+       (uno == 'tijera' and dos == 'papel') or \
+       (uno == 'papel' and dos == 'piedra'):
+        return 1
+    
+    return 2
