aa1289eef6d024fa2cd6c453595e2c3d16e5bcb0
quevedocandela <quevedocandela05@gmail.com>
2025-11-06 11:27:13 -0300

    se puso en practica la funcion del ejercicio 25

---
+++ pareja12/ejercicio25.py
@@ -0,0 +1,6 @@
+from funciones import frecuencia_caracteres
+
+print(frecuencia_caracteres("hola"))
+print(frecuencia_caracteres("banana"))
+print(frecuencia_caracteres("programacion"))
+print(frecuencia_caracteres("aaa bbb"))
